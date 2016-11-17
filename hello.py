494673d1a8dab6f4f2a5f5ad644883426a927da7
mixdjoker <mix.djoker@gmail.com>
2016-11-17 17:50:23 +1000

    downgrade python to 2.7

--- hello.py
+++ hello.py
@@ -1,15 +1,16 @@
-from urllib import parse
+from cgi import parse_qs
 
 
 def application(environ, start_response):
-    query = parse.parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
+    query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
     body = []
     for qs_key, qs_val in query.items():
         for qs_item in qs_val:
-            body.append("{}={}\r\n".format(qs_key, qs_item))
+            body.append(key + "=" + item + "\r\n")
 
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
 
     start_response(status, headers)
     return body
+
