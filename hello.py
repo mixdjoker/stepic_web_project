0a0523bc413f32d0ace09a2d02e0bd542e3e43d5
mixdjoker <mix.djoker@gmail.com>
2016-11-17 18:10:38 +1000

    add app handler

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 from cgi import parse_qs
 
 
-def application(environ, start_response):
+def app(environ, start_response):
     query = parse_qs(environ['QUERY_STRING'], keep_blank_values=True)
     body = []
     for qs_key, qs_val in query.items():
--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
+
+sudo rm /etc/nginx/sites-enabled/*
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default.conf
 sudo /etc/init.d/nginx restart
+sudo rm  /etc/gunicorn.d/*
 sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
